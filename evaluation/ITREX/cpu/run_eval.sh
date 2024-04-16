cac12c91435bb84110358eec4669155981dc6433
Sun, Xuehao <xuehao.sun@intel.com>
2024-04-16 21:17:58 +0800

    test
    Signed-off-by: Sun, Xuehao <xuehao.sun@intel.com>

--- evaluation/ITREX/cpu/run_eval.sh
+++ evaluation/ITREX/cpu/run_eval.sh
@@ -1,4 +1,35 @@
 #!/bin/bash
 set -xe
 
-echo "hello world"
+PATTERN='[-a-zA-Z0-9_]*='
+
+for i in "$@"; do
+    case $i in
+    --config_name=*)
+        config_name=$(echo $i | sed "s/${PATTERN}//")
+        ;;
+    *)
+        echo "Parameter $i not recognized."
+        exit 1
+        ;;
+    esac
+done
+
+git clone https://github.com/intel/intel-extension-for-transformers.git /intel-extension-for-transformers
+shell_path=/intel-extension-for-transformers/examples/huggingface/pytorch/text-generation/quantization/run_generation.py
+
+git clone https://github.com/XuehaoSun/lb_eval.git /lb_eval_backup
+cd /lb_eval_backup
+echo "Running" > status.txt
+git remote set-url origin https://XuehaoSun:$(TOKEN)@github.com/XuehaoSun/lb_eval.git
+git add . && git commit -m "Update status" && git push
+
+cd /lb_eval
+params=$(python parse_config.py $config_name)
+
+cmd="python -u $shell_path --trust_remote_code $params"
+${cmd}
+
+cd /lb_eval_backup
+echo "Finished" > status.txt
+git add . && git commit -m "Update status" && git push
---
+++ parse_config.py
@@ -0,0 +1,33 @@
+import json
+import sys
+
+
+def json_to_command_line(json_data):
+    command_line = []
+    if json_data["precision"] == "8bit":
+        command_line.append(f"--bits 8")
+    elif json_data["precision"] == "4bit":
+        command_line.append(f"--bits 4")
+
+    command_line.append(f"--woq_algo {json_data['quant_type']}")
+    command_line.append(f"--dtype {json_data['weight_dtype']}")
+    command_line.append(f"--model {json_data['model']}")
+    return command_line
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <json_file>")
+        sys.exit(1)
+
+    json_file = sys.argv[1]
+
+    with open(json_file, "r") as file:
+        json_data = json.load(file)
+
+    command_line = json_to_command_line(json_data)
+    print(" ".join(command_line))
+
+
+if __name__ == "__main__":
+    main()
