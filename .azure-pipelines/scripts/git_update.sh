04c896ffa66597da274461bb640aad12d0e5d086
chensuyue <suyue.chen@intel.com>
2024-04-17 10:56:24 +0800

    update run yaml

---
+++ .azure-pipelines/scripts/git_update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -x
+
+git clone https://github.com/XuehaoSun/lb_eval.git lb_eval_backup
+git checkout main
+cd lb_eval_backup/status
+sed -i "s/\"status\":.*/\"status\": \"${status}\"/g" ${requestJson}
+
+git config --global user.email "xuehao.sun@intel.com"
+git config --global user.name "Sun,Xuehao"
+git remote set-url origin https://XuehaoSun:$(TOKEN)@github.com/XuehaoSun/lb_eval.git
+git add . && git commit -m ${commitMessage}
+
+# retry 3 times in case of parallel push conflict
+n=0
+until [ "$n" -ge 3 ]
+do
+  git pull && git push && break
+  n=$((n+1))
+  sleep 3
+done
+
+rm -rf lb_eval_backup
